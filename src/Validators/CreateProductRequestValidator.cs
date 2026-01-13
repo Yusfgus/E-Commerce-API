0d006050c01e256f228941e64accb3b4ccd967cc
UC <yousefgus111@gmail.com>
2026-01-13 20:50:28 +0200

    add Validators

---
+++ src/Validators/CreateProductRequestValidator.cs
@@ -0,0 +1,32 @@
+using E_Commerce.Requests.Product;
+using FluentValidation;
+
+namespace ControllerFluentValidations.Validators;
+
+public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductRequestValidator()
+    {
+        RuleFor(x => x.VendorId)
+            .NotEmpty().WithMessage("Vendor id is requireddddddd");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product Name is required")
+            .Length(3, 255).WithMessage("Product Name must be between 3 and 255 characters");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000).WithMessage("Description can't exceed 1000 characters");
+
+        RuleFor(x => x.Price)
+            .NotEmpty().WithMessage("Product Price is required")
+            .GreaterThanOrEqualTo(0.01m).WithMessage("Price must be at least 0.01");
+
+        RuleFor(x => x.StockQuantity)
+            .NotEmpty().WithMessage("Product Stock Quantity is required")
+            .GreaterThanOrEqualTo(0).WithMessage("Stock Quantity can't be negative");
+    
+        RuleFor(x => x.Category)
+            .NotNull().WithMessage("Product Category is required")
+            .IsInEnum().WithMessage("Invalid product category");
+    }    
+}
